19648e106559af2bf9fe54f4180aa93ceae0352b
Rahul Hegde <rgh9883@rit.edu>
2026-01-31 21:57:16 -0500

    implement everything

--- hw1.py
+++ hw1.py
@@ -1,5 +1,44 @@
 import sys
+from collections import deque
 
+
+# alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+
+def get_neighbors(word: str, word_list: list[str]):
+    neighbors = []
+    for node in word_list:
+        total_diff = 0
+        for i in range(len(word)):
+            if node[i] != word[i]:
+                total_diff += 1
+        if total_diff == 1:
+            neighbors.append(node)
+    return neighbors
+
+def bfs_traversal(start, end, word_list):
+    queue = deque()
+    parent = dict()
+    visited = set()
+    queue.append(start)
+    visited.add(start)
+    while queue:
+        node = queue.popleft()
+        for neighbor in get_neighbors(node, word_list):
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+                parent[neighbor] = node
+                if neighbor == end:
+                    return parent
+    return parent
+
+def build_path(start, end, parent):
+    path = [end]
+    cur_word = end
+    while cur_word != start:
+        cur_word = parent[cur_word]
+        path.insert(0, cur_word)
+    return path
 
 if __name__ == "__main__":
     words_file = sys.argv[1]
@@ -11,7 +50,11 @@ if __name__ == "__main__":
             word = line.strip()
             if len(word) == len(start_str):
                 pos_words.append(word)
-
-    for word in pos_words:
+    parent = bfs_traversal(start_str, target_str, pos_words)
+    if target_str not in parent:
+        print("No solution")
+        sys.exit()
+    path = build_path(start_str, target_str, parent)
+    for word in path:
         print(word)
             
